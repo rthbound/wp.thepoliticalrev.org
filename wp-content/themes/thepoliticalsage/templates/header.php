cea6db4983d04d0d44c936a19b1635b29e01f539
Mason Bourgeois <mason.bourgeois@gmail.com>
2016-11-16 14:31:41 -0600

    Merging in external js and packages

--- wp-content/themes/thepoliticalsage/templates/header.php
+++ wp-content/themes/thepoliticalsage/templates/header.php
@@ -19,24 +19,16 @@
 		<nav>
 			<a href="#menu">Menu</a>
 		</nav>
+		
 	</header>
 
 	<!-- Menu -->
 	<nav id="menu">
-		<ul class="links">
-			{% if page.layout != "home" %}
-					<li><a href="{{ site.baseurl }}/">Home</a></li>
-			{% endif %}
-	    {% for link in site.pages %}
-					{% if link.nav-menu %}
-							<li><a href="{{ site.baseurl }}{{ link.url }}">{{ link.title }}</a></li>
-					{% endif %}
-			{% endfor %}
-			        <!-- <li><a href="{{ site.baseurl }}about/">About</a></li>
-			        <li><a href="{{ site.baseurl }}candidates/">Candidates</a></li>
-							<li><a href="{{ site.baseurl }}/contact.html">Contact Us</a></li> -->
-			        <!-- <li><a href="{{ site.baseurl }}events.html">Events</a></li> -->
-		</ul>
+			<?php
+      if (has_nav_menu('primary_navigation')) :
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'links']);
+      endif;
+      ?>
 		<ul class="actions vertical">
 			<!-- <li><a href="{{ site.url }}{{ site.baseurl }}/vote.html" class="button special fit">Register to Vote</a></li> -->
 			<!-- <li><a href="{{ site.url }}{{ site.baseurl }}/contact.html" class="button fit">Contact Us</a></li> -->
