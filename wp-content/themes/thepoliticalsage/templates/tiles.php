a0c0c26f8cd760bfab3d84f379db8f32d0cbcf7a
Mason Bourgeois <mason.bourgeois@gmail.com>
2016-11-17 00:34:53 -0600

    Fix tile image and url links

--- wp-content/themes/thepoliticalsage/templates/tiles.php
+++ wp-content/themes/thepoliticalsage/templates/tiles.php
@@ -9,13 +9,12 @@
 	<?php $query = new WP_Query( [ 'post_type' => ['post'] ] );
          if ( $query->have_posts() ) : ?>
     <?php while ( $query->have_posts() ) : $query->the_post(); ?>
-        <?php $img = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full'); ?>
           <article>
                   <span class="image">
-                          <img src="<?= $img[0] ?>" alt="" />
+                          <img src="<?= the_post_thumbnail_url('large'); ?>" alt="" />
                   </span>
                   <header class="major">
-                          <h3><a href="{{ site.baseurl }}{{ post.url }}" class="link"><?php the_title(); ?></a></h3>
+                          <h3><a href="<?= get_permalink() ?>" class="link"><?php the_title(); ?></a></h3>
                           <p><?php the_excerpt(); ?></p>
                   </header>
           </article>
