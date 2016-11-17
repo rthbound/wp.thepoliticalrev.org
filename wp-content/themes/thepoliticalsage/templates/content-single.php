efcc89146ca5b423ddc518c53d86012ecb48cd79
Matt Cotter <cotter.m@gmail.com>
2016-11-17 13:30:52 -0500

    Add files via upload (#9)
    * Add files via upload
    
    Added option to set header image to "featured image" in article; if no featured photo is available, rally-compressed.jpg is used. Each post can have its own image now.
    
    Featured image option requires update to functions.php file
    
    Added _posts.scss for styling (relies on bootstrap classes)
    
    entry-meta.php controls author & date details.
    
    Tweet option below each post pulls url & title dynamically.
    
    This area allowing comments by default but is now currently commented out.
    
    * Update functions.php
    
    thumbnails are already enabled from lib/setup.php :)

--- wp-content/themes/thepoliticalsage/templates/content-single.php
+++ wp-content/themes/thepoliticalsage/templates/content-single.php
@@ -1,15 +1,35 @@
+
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
-    <header>
+    <div class="post-banner" style="background-image: url('<?php
+                                   if ( has_post_thumbnail() ) { 
+                                       the_post_thumbnail_url();
+                                   }
+                                   ?>')">
+      <header class="major text-center">
       <h1 class="entry-title"><?php the_title(); ?></h1>
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
-    <div class="entry-content">
-      <?php the_content(); ?>
     </div>
-    <footer>
-      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+    <div class="entry-content">
+    <div class="container-fluid">
+      <div class="col-md-8 text-wrapper">
+      <?php the_content(); ?>
+      <hr />
+      <p>You can comment, follow, and read more from the Political Revolution on <a href="http://www.medium.com/@OurPoliticaRev" target="_blank">Medium</a>. 
+
+      <p>
+        <a class="btn-tweet" target="_blank" href="https://twitter.com/intent/tweet?text=<?php the_title(); ?>&amp;url=<?php echo get_permalink(); ?>&amp;via=OurPoliticalRev" target="_blank">
+          Tweet
+        </a>
+      </p>
+      </div>
+    </div>
+    <!--<footer>
+      <?php //wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
     </footer>
-    <?php comments_template('/templates/comments.php'); ?>
+    <?php //comments_template('/templates/comments.php'); ?>-->
   </article>
+  
 <?php endwhile; ?>
+
