ac214f930852d6b2ce7d0f93e320048ad9801d93
Mason Bourgeois <mason.bourgeois@gmail.com>
2016-11-28 17:38:34 -0600

    Start making petition template

--- wp-content/themes/thepoliticalsage/templates/head.php
+++ wp-content/themes/thepoliticalsage/templates/head.php
@@ -3,4 +3,9 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
+  <?php if (is_page_template('templates/template-petition.php')) : ?>
+  
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-placeholder/2.3.1/jquery.placeholder.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-sheetrock/1.1.1/dist/sheetrock.min.js"></script>
+<?php endif; ?>
 </head>
