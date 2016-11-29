4789b945c5e0bfb72daa2eaed1534ec569031454
Mason Bourgeois <mason.bourgeois@gmail.com>
2016-11-29 11:30:19 -0600

    Reduce excerpt length

--- wp-content/themes/thepoliticalsage/lib/extras.php
+++ wp-content/themes/thepoliticalsage/lib/extras.php
@@ -31,3 +31,9 @@ function excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+/* Change Excerpt length */
+function custom_excerpt_length( $length ) {
+  return 20;
+}
+add_filter('excerpt_length', __NAMESPACE__ . '\\custom_excerpt_length', 999 );
