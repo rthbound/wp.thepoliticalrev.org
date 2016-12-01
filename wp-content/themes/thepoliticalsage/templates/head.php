b607a7d47b2acf403a5049e61bf452a1f157758f
Mason Bourgeois <mason.bourgeois@gmail.com>
2016-11-30 21:04:57 -0600

    Update head.php
    Add google site verification

--- wp-content/themes/thepoliticalsage/templates/head.php
+++ wp-content/themes/thepoliticalsage/templates/head.php
@@ -2,6 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="google-site-verification" content="GeEavsoPBp10LOO68IiuDH16WRABk-3TD1pSs5-dVXU" />
   <?php wp_head(); ?>
   <?php if (is_page_template('templates/template-petition.php')) : ?>
   
