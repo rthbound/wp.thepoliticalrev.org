bc43801cb6d917c324c3a54a859ae7289007410b
Mason Bourgeois <mason.bourgeois@gmail.com>
2016-12-27 01:54:08 -0600

    Hotfix: Social Links

--- wp-content/themes/thepoliticalsage/templates/footer.php
+++ wp-content/themes/thepoliticalsage/templates/footer.php
@@ -13,15 +13,15 @@
     <footer id="footer">
             <div>
                 <ul class="icons">
-                    <li><a href="{{ site.twitter_url }}" class="icon fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
-                    <li><a href="{{ site.facebook_url }}" class="icon fa-facebook" target="_blank"><span class="label">Facebook</span></a></li>
+                    <li><a href="https://twitter.com/ourpoliticalrev" class="icon fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
+                    <li><a href="https://www.facebook.com/ThePoliticalRev/" class="icon fa-facebook" target="_blank"><span class="label">Facebook</span></a></li>
                     <!-- {% if site.gitlab_url %}
                     <li><a href="{{ site.gitlab_url }}" class="icon fa-gitlab" target="_blank"><span class="label">GitLab</span></a></li>
                     {% endif %} -->
-                    <li><a href="{{ site.github_url }}" class="icon fa-github" target="_blank"><span class="label">GitHub</span></a></li>
-                    <li><a href="{{ site.medium_url }}" class="icon fa-medium" target="_blank"><span class="label">Medium</span></a></li>
-                    <li><a href="{{ site.slack_url }}" class="icon fa-slack" target="_blank"><span class="label">Slack</span></a></li>
-                    <li><a href="{{ site.subreddit_url }}" class="icon fa-reddit-alien" target="_blank"><span class="label">LinkedIn</span></a></li>
+                    <li><a href="https://github.com/politicalrev" class="icon fa-github" target="_blank"><span class="label">GitHub</span></a></li>
+                    <li><a href="https://medium.com/voices-of-the-political-revolution/the-smear-is-here-for-keith-ellison-651c9be0992c#.vbklzk4ae" class="icon fa-medium" target="_blank"><span class="label">Medium</span></a></li>
+                    <li><a href="https://polrevvols.herokuapp.com/" class="icon fa-slack" target="_blank"><span class="label">Slack</span></a></li>
+                    <li><a href="https://www.reddit.com/r/Political_Revolution/" class="icon fa-reddit-alien" target="_blank"><span class="label">LinkedIn</span></a></li>
                 </ul>
             </div>
             <div>
