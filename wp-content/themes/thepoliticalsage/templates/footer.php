eb9a1b7b4a32234dadc2414c7142ca5c1bdcc3d0
Mason Bourgeois <mason.bourgeois@gmail.com>
2016-12-27 01:10:20 -0600

    Footer (#16)
    * Update social links and add nav links
    
    * Continue styling footer
    
    * Finish implementing footer except for color
    
    Subject to mobile design approval

--- wp-content/themes/thepoliticalsage/templates/footer.php
+++ wp-content/themes/thepoliticalsage/templates/footer.php
@@ -11,22 +11,29 @@
     </footer>-->
 
     <footer id="footer">
-            <ul class="icons">
-                <li><a href="{{ site.twitter_url }}" class="icon alt fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
-                <li><a href="{{ site.facebook_url }}" class="icon alt fa-facebook" target="_blank"><span class="label">Facebook</span></a></li>
-                <!-- {% if site.gitlab_url %}
-                <li><a href="{{ site.gitlab_url }}" class="icon alt fa-gitlab" target="_blank"><span class="label">GitLab</span></a></li>
-                {% endif %} -->
-                <li><a href="{{ site.github_url }}" class="icon alt fa-github" target="_blank"><span class="label">GitHub</span></a></li>
-                <li><a href="{{ site.medium_url }}" class="icon alt fa-medium" target="_blank"><span class="label">Medium</span></a></li>
-                <li><a href="{{ site.slack_url }}" class="icon alt fa-slack" target="_blank"><span class="label">Slack</span></a></li>
-                <li><a href="{{ site.subreddit_url }}" class="icon alt fa-reddit-alien" target="_blank"><span class="label">LinkedIn</span></a></li>
-            </ul>
-            <ul class="copyright">
-                <li>&copy; <?= bloginfo('title'); ?></li>
-                <!-- <li>Design: <a href="https://html5up.net" target="_blank">HTML5 UP</a></li> -->
-
-            </ul>
+            <div>
+                <ul class="icons">
+                    <li><a href="{{ site.twitter_url }}" class="icon fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
+                    <li><a href="{{ site.facebook_url }}" class="icon fa-facebook" target="_blank"><span class="label">Facebook</span></a></li>
+                    <!-- {% if site.gitlab_url %}
+                    <li><a href="{{ site.gitlab_url }}" class="icon fa-gitlab" target="_blank"><span class="label">GitLab</span></a></li>
+                    {% endif %} -->
+                    <li><a href="{{ site.github_url }}" class="icon fa-github" target="_blank"><span class="label">GitHub</span></a></li>
+                    <li><a href="{{ site.medium_url }}" class="icon fa-medium" target="_blank"><span class="label">Medium</span></a></li>
+                    <li><a href="{{ site.slack_url }}" class="icon fa-slack" target="_blank"><span class="label">Slack</span></a></li>
+                    <li><a href="{{ site.subreddit_url }}" class="icon fa-reddit-alien" target="_blank"><span class="label">LinkedIn</span></a></li>
+                </ul>
+            </div>
+            <div>
+                <span class="copyright">&copy; <?= bloginfo('title'); ?> 2017</span>
+                <img src="<?= get_stylesheet_directory_uri(); ?>/assets/images/button.png" />
+                <ul class="footernav">
+                    <li>Contact Us</li>
+                    <li>Search</li>
+                    <li>By-laws</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
              <?php if (is_page_template('templates/template-petition.php')) : ?>
                 <script>
                     $(document).ready(function(){
