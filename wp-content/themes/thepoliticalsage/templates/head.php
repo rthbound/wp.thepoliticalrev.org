06097a16cad3c04f11b5d89b1a761b5fb59fd5c1
Mason Bourgeois <mason.bourgeois@gmail.com>
2016-12-28 01:30:44 -0600

    Theming updates: Teal and Work Sans

--- wp-content/themes/thepoliticalsage/templates/head.php
+++ wp-content/themes/thepoliticalsage/templates/head.php
@@ -3,6 +3,7 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="google-site-verification" content="GeEavsoPBp10LOO68IiuDH16WRABk-3TD1pSs5-dVXU" />
+  <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500" rel="stylesheet">
   <?php wp_head(); ?>
   <?php if (is_page_template('templates/template-petition.php')) : ?>
   
