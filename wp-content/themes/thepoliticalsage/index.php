fb9d62cfcf0e3bd57b50095a114249b2e98d7d60
Mason Bourgeois <mason.bourgeois@gmail.com>
2017-01-02 21:38:08 -0600

    Hotfix: remove banner from articles page

--- wp-content/themes/thepoliticalsage/index.php
+++ wp-content/themes/thepoliticalsage/index.php
@@ -1,21 +1,6 @@
 <!-- Banner -->
 <?php $img = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full'); ?>
 
-<section id="banner" class="major" style="background-position: center 0px; background-image: url(http://i.huffpost.com/gen/3431754/images/o-BERNIE-SANDERS-facebook.jpg);">
-    <div class="inner">
-        <!-- <span class="image" style="display: none;">
-            <img src="images/pic07.jpg" alt="">
-        </span> -->
-        <header class="major">
-            <?php get_template_part('templates/page', 'header'); ?>
-        </header>
-        <div class="content">
-            <p>Lorem ipsum dolor sit amet nullam consequat<br>
-            sed veroeros. tempus adipiscing nulla.</p>
-        </div>
-    </div>
-</section>
-
 <!-- Main -->
 <main>
 
